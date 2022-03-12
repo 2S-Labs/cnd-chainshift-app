a92136b6c6a14238eb1cb2fd8e658c60df5a43ac
elixirevo <elixirevocation@gmail.com>
2022-03-12 18:20:58 +0900

    update: contract address

--- src/contracts/index.ts
+++ src/contracts/index.ts
@@ -2,8 +2,8 @@ import { ethers } from 'ethers'
 import { signer } from '@/chain/index'
 import ChainShiftABI from '@/abi/ChainShift.json'
 
-const chainShiftV1CA = ''
-const chainShiftV2CA = '0x574bed967Ba7ac8aE9d57044aF637b439b81FA53'
+const chainShiftV1CA = '0x8FCdD6DD063570Ec81Cba14d8CC35Dbf828dEC88'
+const chainShiftV2CA = '0x5DC00aF0874215Dd9fCb066097144be298804DDB'
 
 export async function inputV1(tokenIds: Array<any>, to: string) {
   const contract = new ethers.Contract(chainShiftV1CA, ChainShiftABI, signer)
